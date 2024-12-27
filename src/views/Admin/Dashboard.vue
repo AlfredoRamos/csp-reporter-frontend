4edab490358dc0dbfdbbcf87c6a9dca2c964db2e
Alfredo Ramos <alfredo.ramos@proton.me>
2024-12-27 03:18:29 +0000

    Fix view auth guard

--- src/views/Admin/Dashboard.vue
+++ src/views/Admin/Dashboard.vue
@@ -1,19 +1,27 @@
 <script setup>
 import { ref, onBeforeMount } from 'vue';
+import { useRouter } from 'vue-router';
 import { useAuthStore } from '@/stores/auth';
 import Authenticated from '@/layouts/Authenticated.vue';
 import Loader from '@/components/Loader.vue';
 
+const router = useRouter();
 const auth = useAuthStore();
 const loading = ref(true);
 
 onBeforeMount(async () => {
-	auth?.guard();
-
-	// TODO: Make real API call
-	setTimeout(() => {
-		loading.value = false;
-	}, 500);
+	auth?.guard()
+		.then(() => {
+			// TODO: Make real API call
+			setTimeout(() => {
+				loading.value = false;
+			}, 500);
+		})
+		.catch(() => {
+			loading.value = false;
+			router.push({ name: 'auth_login' });
+			return;
+		});
 });
 </script>
 
